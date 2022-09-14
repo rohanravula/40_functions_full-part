3c6575fa9ff39f2a45817b1f0fe966e94045e95f
rohanravula <109545469+rohanravula@users.noreply.github.com>
2022-09-14 23:23:27 +0530

    Add files via upload

---
+++ 4_default_arf_function.py
@@ -0,0 +1,8 @@
+def info(name="hey",age=30):
+    print("name:",name)
+    print("age:",age)
+# info()
+# info("py") # this is called modification hey will change to py
+# info(name="gopal") #it changes the name
+# info(age=45) # it changes the age
+info("ravi",80) #by using this we can change the name and age at a time
